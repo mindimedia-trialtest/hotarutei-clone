793ce3b7cde8d2b8d8fa4d12f7ae9ced92b56b5e
Tjoet Muty Ahmad <tjmutyahmd27@gmail.com>
2026-02-19 23:04:42 +0800

    add new item

---
+++ src/constants/index.js
@@ -0,0 +1,20 @@
+export const Buttons = [
+  { id: 1, title: "Room Reservation" },
+  { id: 2, title: "Meal Reservation" },
+];
+
+export const Menus = [
+  { id: 1, title: "Information" },
+  { id: 2, title: "Overview" },
+  { id: 3, title: "Restaurant" },
+  { id: 4, title: "Stay" },
+  { id: 5, title: "Access" },
+  { id: 6, title: "FAQ" },
+  { id: 7, title: "Contact" },
+];
+
+export const Infos = [
+  {id: 1, time: "2025.07.22", type: "(Restaurant)", desc: "New Lunch Set Launching Soon"},
+  {id: 2, time: "2024.12.18", type: "Hotarutei Restaurant"},
+  {id: 3, time: "2025.07.22", type: "(Notice)", desc: "Notice of October 2024 'Closed Days'"},
+]
