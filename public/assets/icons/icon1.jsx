5c5ced4379c6f1a52d314e1a9d8d29863c82c095
Tjoet Muty Ahmad <tjmutyahmd27@gmail.com>
2026-02-19 23:04:21 +0800

    add icons and image

---
+++ public/assets/icons/icon1.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+const Icon1 = () => {
+  return (
+    <div>
+      <svg
+        width="15"
+        height="50"
+        viewBox="0 0 40 120"
+        fill="none"
+        stroke="currentColor"
+        strokeWidth="3"
+      >
+        <line x1="20" y1="20" x2="20" y2="90" />
+        <line x1="20" y1="70" x2="1" y2="60" />
+      </svg>
+    </div>
+  );
+};
+
+export default Icon1;
---
+++ public/assets/icons/icon2.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+const Icon2 = () => {
+  return (
+    <svg
+      width="15"
+      height="100"
+      viewBox="0 0 40 120"
+      fill="none"
+      stroke="currentColor"
+      strokeWidth="3"
+    >
+      <line x1="20" y1="20" x2="20" y2="90" />
+      <line x1="20" y1="70" x2="1" y2="60" />
+    </svg>
+  );
+};
+
+export default Icon2;
