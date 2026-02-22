3f6a09a1a7af2e422f08c75f433222f4876d25f5
Tjoet Muty Ahmad <tjmutyahmd27@gmail.com>
2026-02-22 11:53:13 +0800

    feat: slicing ui navbar section

---
+++ src/features/Navbar/index.jsx
@@ -0,0 +1,31 @@
+import { Buttons } from "@/constants";
+import Image from "next/image";
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <div className="flex justify-between px-6 items-center py-4 bg-[#f8f8f6]">
+      <div className="flex flex-col lg:flex-row md:flex-row items-center gap-2 lg:gap-10">
+        <Image
+          src="/assets/hotarutei-1.png"
+          width={145}
+          height={145}
+          alt="Hotarutei"
+          style={{ width: "auto", height: "auto" }}
+        />
+        <p className="font-semibold text-lg">Embrace 170 years of tradition</p>
+      </div>
+      <div className="hidden lg:flex gap-2 font-serif ">
+        {Buttons.map((button) => (
+          <div key={button.id}>
+            <button className="border-2 border-black px-2 tracking-wider">
+              {button.title}
+            </button>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
